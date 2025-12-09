1725e29fdf09e94c77a323071b576e6b3fa2ce26
1NTROO <95148848+1NTROO@users.noreply.github.com>
2025-12-09 18:23:47 +0100

    build commit

--- 2DPlatform-Prototype1/Assets/Scripts/Mechanics/PlayerCollision.cs
+++ 2DPlatform-Prototype1/Assets/Scripts/Mechanics/PlayerCollision.cs
@@ -4,11 +4,19 @@ namespace Platformer.Mechanics
 {
     public class PlayerCollision : MonoBehaviour
     {
+        [SerializeField] TMPro.TextMeshProUGUI lapsText;
+        private float laps = 0;
+
         private Rigidbody2D rb;
         private Collider2D coll;
         void Start()
         {
-            
+            rb = GetComponent<Rigidbody2D>();
+            coll = GetComponent<Collider2D>();
+            if (lapsText != null)
+            {
+                lapsText.text = laps.ToString() + " Laps";
+            }
         }
 
         void Update()
@@ -18,11 +26,21 @@ namespace Platformer.Mechanics
 
         void OnCollisionEnter2D(Collision2D collision)
         {
-            print("collision detected");
             if (collision.gameObject.CompareTag("DNI"))
             {
-                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(2, 7), Random.Range(3, 11)), ForceMode2D.Impulse);
-                print("DNI hit");
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(2, 5), Random.Range(3, 8)), ForceMode2D.Impulse);
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("FinishLine"))
+            {
+                if (lapsText != null)
+                {
+                    laps++;
+                    lapsText.text = laps.ToString() + " Laps";
+                }
             }
         }
     }
